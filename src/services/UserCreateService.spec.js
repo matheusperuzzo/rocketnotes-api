8f55c4285bcf85c9419e2b0aef631f9ad8f4b742
Matheus Peruzzo <matheusperuzzodev@gmail.com>
2024-10-26 21:49:46 -0400

    Add describe to UserCreateService tests

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,16 +1,18 @@
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 const UserCreateService = require("./UserCreateService");
 
-it("user should be created", async () => {
-  const user = {
-    name: "Teste",
-    email: "teste@mail.com",
-    password: "password",
-  };
+describe("UserCreateService", () => {
+  it("user should be created", async () => {
+    const user = {
+      name: "Teste",
+      email: "teste@mail.com",
+      password: "password",
+    };
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
 
-  expect(userCreated).toHaveProperty("id");
+    expect(userCreated).toHaveProperty("id");
+  });
 });
